eb702c90024ea971edddde406e7435f94e0024dc
David Howden <dhowden@gmail.com>
2022-09-21 09:09:54 +1000

    fixed up YAML formatting

--- envsvc/podspec_test.go
+++ envsvc/podspec_test.go
@@ -12,29 +12,71 @@ kind: Pod
 metadata:
   name: podname
   labels:
-	 name: podname
+    name: podname
 spec:
   containers:
-	 - name: one
-		image: one
-		ports:
-		  - name: grpc
-			 protocol: TCP
-			 containerPort: 50551
-		resources:
-		  requests:
-			 cpu: 100m
-			 memory: 50Mi
-		  limits:
-			 cpu: "1"
-			 memory: 50Mi
-		env:
-		  - name: ONE_BOOLEAN
-			 value: "false"
-		  - name: ONE_STRING
-			 value: "string value"
-		  - name: ONE_INTEGER
-			 value: "1"`
+  - name: one
+    image: one
+    ports:
+    - name: grpc
+      protocol: TCP
+      containerPort: 50551
+    resources:
+      requests:
+        cpu: 100m
+        memory: 50Mi
+      limits:
+        cpu: "1"
+        memory: 50Mi
+    env:
+    - name: ONE_BOOLEAN
+      value: "false"
+    - name: ONE_STRING
+      value: "string value"
+    - name: ONE_INTEGER
+      value: "1"`
+
+const multipleInput = `apiVersion: v1
+kind: Pod
+metadata:
+  name: podname
+  labels:
+    name: podname
+spec:
+  containers:
+  - name: one
+    image: one
+    ports:
+    - name: grpc
+      protocol: TCP
+      containerPort: 50551
+    resources:
+      requests:
+        cpu: 100m
+        memory: 50Mi
+      limits:
+        cpu: "1"
+        memory: 50Mi
+    env:
+    - name: ONE_BOOLEAN
+      value: "false"
+    - name: ONE_STRING
+      value: "string value"
+    - name: ONE_INTEGER
+      value: "1"
+  - name: two
+    image: two
+    ports:
+    - name: grpc
+      protocol: TCP
+      containerPort: 50552
+    env:
+    - name: TWO_BOOLEAN
+      value: "true"
+    - name: TWO_STRING
+      value: "string value two"
+    - name: TWO_INTEGER
+      value: "2"`
 
 func TestPodENVLookup(t *testing.T) {
 	t.Run("single_container", func(t *testing.T) {
@@ -55,49 +97,7 @@ func TestPodENVLookup(t *testing.T) {
 	})
 
 	t.Run("multiple_containers", func(t *testing.T) {
-		input := `apiVersion: v1
-kind: Pod
-metadata:
-	name: podname
-	labels:
-		name: podname
-spec:
-	containers:
-		- name: one
-		image: one
-		ports:
-			- name: grpc
-				protocol: TCP
-				containerPort: 50551
-		resources:
-			requests:
-				cpu: 100m
-				memory: 50Mi
-			limits:
-				cpu: "1"
-				memory: 50Mi
-		env:
-			- name: ONE_BOOLEAN
-				value: "false"
-			- name: ONE_STRING
-				value: "string value"
-			- name: ONE_INTEGER
-				value: "1"
-		- name: two
-		image: two
-		ports:
-			- name: grpc
-				protocol: TCP
-				containerPort: 50552
-		env:
-			- name: TWO_BOOLEAN
-				value: "true"
-			- name: TWO_STRING
-				value: "string value two"
-			- name: TWO_INTEGER
-				value: "2"`
-
-		x, err := envsvc.PodENVLookup(strings.NewReader(input), "two")
+		x, err := envsvc.PodENVLookup(strings.NewReader(multipleInput), "two")
 		must(t, err)
 
 		expectValue(t, x, "TWO_BOOLEAN", "true")
